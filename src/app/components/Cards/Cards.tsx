0130cb375cbd5e107f6496c6d9c8d9ae546a9f94
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-04-09 17:38:50 -0300

    feat: add cards connection to API in Cards Component

--- src/app/components/Cards/Cards.tsx
+++ src/app/components/Cards/Cards.tsx
@@ -1,50 +1,55 @@
 'use client'
+import { RootState } from '@/app/store';
+import { getWeatherData, handleSelectForecastDay } from '@/app/store/weatherSlice';
 import { SunIcon } from '@heroicons/react/24/solid'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { getDay, format } from 'date-fns'
+import { AnyAction } from '@reduxjs/toolkit';
 
-interface CardProps {
-  id: number;
-  date: string;
-  day: string;
-  hour: string;
-  weather: string;
-}
+let weekDays = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"]
 
-const MOCKED_CARDS: CardProps[] = [
-  { id: 0, day: 'Segunda', date:'10/04', hour: '11:42', weather: '16' },
-  { id: 1, day: 'Terça', date:'11/04', hour: '11:42', weather: '20' },
-  { id: 2, day: 'Quarta', date:'12/04', hour: '11:42', weather: '16' },
-  { id: 3, day: 'Quinta', date:'13/04', hour: '11:42', weather: '20' },
-  { id: 4, day: 'Sexta', date:'14/04', hour: '11:42', weather: '16' },
-  { id: 5, day: 'Sábado', date:'15/04', hour: '11:42', weather: '20' },
-  { id: 6, day: 'Domingo', date:'16/04', hour: '11:42', weather: '16' },
-];
+export default function Cards() {
+  const { location, current, forecast } = useSelector(({ weather }: RootState) => weather)
+  const dispatch = useDispatch()
 
-const CITY = "Santos";
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=Santos&days=7&aqi=no&alerts=no`);
+    
+      const data = await response.json().then((res) => dispatch(getWeatherData(res)))
+      console.log(data)
+      return data
+    }
 
-export default async function Cards() {
-  const [cards, setCards] = useState<CardProps[]>(MOCKED_CARDS);
-  
-  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_API_KEY}&q=${CITY}&days=7&aqi=no&alerts=no`);
+    getData()
+  }, [])
 
-  const data = response.json();
-
-  console.log('data: ', data);
+  const handleClickCard = (forecastDay: any): AnyAction => {
+    return dispatch(handleSelectForecastDay(forecastDay))
+  }
 
   return (
-    <div className="flex lg:flex-row flex-col py-10">
-      {cards.map((card) => (
-        <div key={`card-${card.id}`} className="bg-gradient-to-r from-sky-200 to-sky-300 w-full h-48 rounded-3xl flex flex-col justify-center items-center mr-4 md:mb-10 sm:mb-10 cursor-pointer">
-        <div className="flex flex-col">
-          <h1 className="text-3xl font-bold text-zinc-700">{card.day}</h1>
-          <h3 className="text-1xl text-zinc-500 font-semibold self-end">{card.date}</h3>
+    <>
+      <h3 className="text-zinc-200 text-2xl font-bold">Se liga no clima em {location?.name}:</h3>
+      <div className="flex lg:flex-row flex-col py-10">
+        {forecast && forecast.forecastday?.map((weather: any) => (
+          <div
+            key={`card-${weather.date}`}
+            className="bg-gradient-to-r from-sky-200 to-sky-300 w-full h-48 rounded-3xl flex flex-col justify-center items-center mr-4 md:mb-10 sm:mb-10 cursor-pointer"
+            onClick={() => handleClickCard(weather)}
+          >
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-bold text-zinc-700">{weekDays[getDay(new Date(weather.date))]}</h1>
+            <h3 className="text-1xl text-zinc-500 font-semibold self-end">{format(new Date(weather.date), 'dd/MM')}</h3>
+          </div>
+          <div className="flex flex-row items-center">
+            <h3 className="text-zinc-700 text-2xl font-bold">{weather.day.avgtemp_c}º</h3>
+            <SunIcon className="w-12 h-12 text-yellow-500" />
+          </div>
         </div>
-        <div className="flex flex-row items-center">
-          <h3 className="text-zinc-700 text-2xl font-bold">{card.weather}º</h3>
-          <SunIcon className="w-12 h-12 text-yellow-500" />
-        </div>
+        ))}
       </div>
-      ))}
-    </div>
+    </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Cards, DetailsCard } from './components';
 
-export default function Home() {
+export default async function Home() {
   return (
-    <div className='flex flex-col items-center bg-zinc-700 md:max-h-auto sm:max-h-auto lg:h-screen justify-between'>
+    <div className='flex flex-col items-center bg-zinc-700 md:max-h-auto sm:max-h-auto lg:h:full justify-between'>
       <div className="flex lg:flex-row sm:flex-col w-full justify-between p-10">
         <div className="flex flex-col p-10 w-9/12">
-        <h3 className="text-zinc-200 text-2xl font-bold">Se liga no clima em Santos:</h3>
         <Cards />
 
         <DetailsCard />
